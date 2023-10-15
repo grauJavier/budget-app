77f6c7915b9c0366bd1d5aebb9a38d571e3144d6
Javier Grau <javiergrau.freelance@gmail.com>
2023-10-15 18:33:39 -0300

    Initialize 'Devise' authentification

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+
+  devise_for :users, path: 'auth', path_names: {
+  sign_in: 'login',
+  sign_out: 'logout',
+  password: 'secret',
+  confirmation: 'verification'
+}
 end
